fff5b565e733ca0641bcdc698dd0a187eb30db27
Yurii <y.volkov@dunice.net>
2022-02-08 11:47:38 +0300

    server migrations, tables, models - done

---
+++ server/app/Models/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = 'news';
+
+    protected $fillable = ['head', 'text', 'author_id', 'tags', 'image'];
+}
