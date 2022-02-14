b347a60baa67e54288baab85fe82a5cf029cc77e
Yurii <y.volkov@dunice.net>
2022-02-14 10:46:00 +0300

    fali system done, store.js added

---
+++ client/src/redux/store/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import mySagas from '../sagas';
+import reducer from '../reducers/rootReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware)),
+);
+
+sagaMiddleware.run(mySagas);
+export default store;
