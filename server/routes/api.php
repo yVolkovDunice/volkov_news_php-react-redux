74ae7f97374bf272fe05710525233d3bc32657c3
Yurii <y.volkov@dunice.net>
2022-03-11 17:56:42 +0300

    added UserControlle

--- server/routes/api.php
+++ server/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\User\UserController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,11 +18,9 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get('/user', [UserController::class, 'index']);
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/posts', [PostController::class, 'getPosts']);
